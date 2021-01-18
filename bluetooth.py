7489980379f0a40c7e5cd7ee66f0619cba81a022
George Gebbett <george.gebbett1@gmail.com>
2021-01-18 12:52:14 +0000

    create bluetooth tester

--- bluetooth.py
+++ bluetooth.py
@@ -7,15 +7,13 @@ def lookForFastForward():
         if not path.exists('/dev/input/event0'):
             break
         else:
-            try:
-                speakerButtons = evdev.InputDevice('/dev/input/event0')
-                event = speakerButtons.read()
-                if type(event) != type(None):
-                    if evdev.events.KeyEvent(event).keystate == 1:
-                        if evdev.events.KeyEvent(event).keycode == "KEY_NEXTSONG":
-                            print("Playing next song")
-            except:
-                pass
+            speakerButtons = evdev.InputDevice('/dev/input/event0')
+            event = speakerButtons.read()
+            if type(event) != type(None):
+                if evdev.events.KeyEvent(event).keystate == 1:
+                    if evdev.events.KeyEvent(event).keycode == "KEY_NEXTSONG":
+                        print("Playing next song")
+
 
 
 lookForFastForward()
