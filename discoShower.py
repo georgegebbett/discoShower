796817c8c7fb3d346640466adf63ce4d2d3f6394
George Gebbett <george.gebbett1@gmail.com>
2021-01-18 12:33:51 +0000

    no longer using the read loop for getting speaker button events

--- discoShower.py
+++ discoShower.py
@@ -121,16 +121,17 @@ if useThreading:
         while True:
             if not path.exists('/dev/input/event0'):
                 break
-            try:
-                speakerButtons = evdev.InputDevice('/dev/input/event0')
-                event = speakerButtons.read()
-                if type(event) != type(None):
-                    if evdev.events.KeyEvent(event).keystate == 1:
-                        if evdev.events.KeyEvent(event).keycode == "KEY_NEXTSONG":
-                            spotify.next_track()
-                            print("Playing next song")
-            except:
-                pass
+            else:
+                try:
+                    speakerButtons = evdev.InputDevice('/dev/input/event0')
+                    event = speakerButtons.read()
+                    if type(event) != type(None):
+                        if evdev.events.KeyEvent(event).keystate == 1:
+                            if evdev.events.KeyEvent(event).keycode == "KEY_NEXTSONG":
+                                spotify.next_track()
+                                print("Playing next song")
+                except:
+                    pass
 
 
 if __name__ == "__main__":
