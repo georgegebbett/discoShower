db635dad7bca3ed5fcfbc6e65e0f82aee48007d3
George Gebbett <george.gebbett1@gmail.com>
2021-01-18 10:16:57 +0000

    starting on #4 with some print statement debugging

--- discoShower.py
+++ discoShower.py
@@ -110,7 +110,7 @@ if useThreading:
                 speakerButtons = evdev.InputDevice('/dev/input/event0')
                 errorPrinted = False
                 for event in speakerButtons.read_loop():
-                    print(evdev.events.KeyEvent(event).key_up)
+                    print(evdev.events.KeyEvent(event).keystate)
                     print(evdev.events.KeyEvent(event).keycode)
                     if evdev.events.KeyEvent(event).key_up == 0:
                         if evdev.events.KeyEvent(event).keycode == "KEY_NEXTSONG":
